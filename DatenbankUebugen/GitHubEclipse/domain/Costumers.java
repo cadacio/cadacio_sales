9bd2d6ccdec2662d478cb045124d2d7bd888966b
htlspengergasse <billy.cadacio@gmail.com>
2016-05-18 16:50:34 +0200

    Customers, Costumers, ArrayList

---
+++ DatenbankUebugen/GitHubEclipse/domain/Costumers.java
@@ -0,0 +1,16 @@
+package domain;
+
+import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+
+
+public class Costumers {
+	
+	private ArrayList <Costumer> costumers;
+
+	public Costumers() {
+		costumers = new ArrayList<Costumer>();
+	}
+
+}
