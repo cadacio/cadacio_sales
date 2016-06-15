7c6fa30336d5125487c6eae53137a6f79e7e9e82
htlspengergasse <billy.cadacio@gmail.com>
2016-06-15 18:25:14 +0200

    Customer

---
+++ DatenbankUebugen/src/domain/Customers.java
@@ -0,0 +1,16 @@
+package domain;
+
+import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+
+
+public class Customers {
+	
+	private ArrayList <Costumer> costumers;
+
+	public Customers() {
+		costumers = new ArrayList<Costumer>();
+	}
+
+}
